060e9649434e462342757ee2ed688d14e61034e4
Oscar Castillo <oscar_97.10@outlook.com>
2024-04-14 22:39:01 -0600

    Add clojure lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,18 +4,26 @@ lsp.preset({
   name = 'minimal',
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+  suggest_lsp_servers = true,
 })
 
 lsp.ensure_installed({
-  'tsserver',
+  'clojure_lsp',
   'eslint',
-  'lua_ls',
-  'rust_analyzer',
-  'jdtls',
   'gopls',
-  'pyright'
+  'jdtls',
+  'lua_ls',
+  'pyright',
+  'rust_analyzer',
+  'tsserver',
 })
+
+lsp.setup_servers({'dartls', force=true})
+
+if vim.fn.executable("flutter") == 1 then
+    require('flutter-tools').setup({})
+end
+
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua-language-server', {
@@ -49,16 +57,17 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>df", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-H>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 
